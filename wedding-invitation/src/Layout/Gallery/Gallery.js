4d6c501f296a740d9cb68814910c6916d88ee25d
clakika <31564756+clakika@users.noreply.github.com>
2020-09-03 10:55:19 +0200

    header of story section added

--- wedding-invitation/src/Layout/Gallery/Gallery.js
+++ wedding-invitation/src/Layout/Gallery/Gallery.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './Gallery.css';
 
+import storyOne from '../../img/storyOne.jpg';
+
 export class Gallery extends Component {
     render() {
         return (
-            <div>
-                <h1>I AM GALLERY!</h1>
+            <div className="Gallery">
+                <header className="HeaderGallery">
+                    <div>
+                        <div className="StoryImg"></div>
+                    </div>
+                    <h1>Our Story</h1>
+                </header>
             </div>
         )
     }
